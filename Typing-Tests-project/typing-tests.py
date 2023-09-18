b38646a6683055a3f2525d2bfa469feba30745f2
Kiavash <Kiavashtaherian@gmail.com>
2023-09-18 09:35:52 +0330

    importing modules and testcases

--- Typing-Tests-project/typing-tests.py
+++ Typing-Tests-project/typing-tests.py
@@ -1,1 +1,15 @@
+from time import time, sleep
+from random import choice
+from colorama import  Fore
 
+# -------------------------------------------------------------------
+
+print(Fore.RED + 'Typing Speedometer')
+print(Fore.WHITE)
+
+Tests = ["Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live.",
+"A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring whi",
+"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was bo"]
+
+x = choice(Tests) 
+print("Your test case is: ", x)
